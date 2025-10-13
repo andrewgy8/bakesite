75787d12682726d8f0a1619efc1e7ff7b43e29c5
Andrew Graham-Yooll <andrewgy8@gmail.com>
2025-10-13 09:15:31 +0200

    Add reuse socket

--- src/bakesite/server.py
+++ src/bakesite/server.py
@@ -13,6 +13,7 @@ def serve(port=DEFAULT_PORT):
         http.server.SimpleHTTPRequestHandler, directory="./_site"
     )
 
+    socketserver.TCPServer.allow_reuse_address = True
     with socketserver.TCPServer(("", port), Handler) as httpd:
         click.echo(f"Serving your baked site at port http://localhost:{port}")
         httpd.serve_forever()
